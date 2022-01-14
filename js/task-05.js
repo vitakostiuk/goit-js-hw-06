53d736294f29e07b240de95439b0e351e28ce85c
Vita Kostiuk <vitagrebennik@gmail.com>
2022-01-14 13:11:50 +0200

    task-5

---
+++ js/task-05.js
@@ -0,0 +1,21 @@
+// Задание 5
+// Напиши скрипт который, при наборе текста в инпуте
+// input#name - input(событие input), подставляет его
+// текущее значение в span#name - output.
+// Если инпут пустой, в спане должна отображаться
+// строка "Anonymous".
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+const inputRef = document.querySelector('#name-input');
+const outputRef = document.querySelector('#name-output');
+
+inputRef.addEventListener('input', () => {
+  console.log(inputRef.value);
+  outputRef.textContent = inputRef.value;
+
+  if (inputRef.value === '') {
+    outputRef.textContent = 'Anonymous';
+  }
+});
